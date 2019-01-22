585950e70b4f1f5b817662fd5e22c194571b02b0
Narongdee <46166349+Narongdee@users.noreply.github.com>
2019-01-22 15:07:43 +0700

    Add files via upload

---
+++ CircurLinklist/src/iData.java
@@ -0,0 +1,13 @@
+public class iData {
+	public int num;
+public iData next,prev;
+	
+	public  iData (int num) {
+		this.num = num ;
+		
+	}
+	
+	public void displayLink() {
+		System.out.println("Number : "+this.num );
+	}
+}
